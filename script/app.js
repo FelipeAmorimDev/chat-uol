46bf7964bd7090d881ce80a7ba8832ee9336d1a3
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-17 21:41:08 -0300

    Funcoes refatoradas

--- script/app.js
+++ script/app.js
@@ -4,38 +4,63 @@ const formElement = document.querySelector('.joinchat__form-join form')
 const joinChatSection = document.querySelector('.joinchat-section')
 const messageForm = document.querySelector('.footer__container form')
 
-async function joinChatRoom(name) {
-  let options = {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: `{"name": "${name}"}`
+const loginHandle = event => {
+  const username = formElement.user.value
+
+  event.preventDefault()
+  tryJoinChat(username)
+}
+
+const renderChatmsgByType = msg => {
+  const userName = formElement.user.value
+
+  const statusTemplate = `<span class="time-style">${msg.time}</span> <span class="text-bold">${msg.from}</span> ${msg.text}..`
+  const msgTemplate = `<span class="time-style">${msg.time}</span> <span class="text-bold">${msg.from}</span>  para <span class="text-bold">${msg.to}</span> : ${msg.text}`
+  const privateMsgTemplate = `<span class="time-style">${msg.time}</span> <span class="text-bold">${msg.from}</span> reservadamente para <span class="text-bold">${msg.to}</span> : ${msg.text}`
+
+  if (msg.type === 'status') {
+    insertElementIntoDOM('chat-msg__item status-msg', statusTemplate)
+  } else if (msg.type === 'message') {
+    insertElementIntoDOM('chat-msg__item normal-msg', msgTemplate)
+
+    return
   }
 
-  const tryingJoinChar = await fetch('https://mock-api.driven.com.br/api/v6/uol/participants', options)
-
-  if (tryingJoinChar.status === 200) {
-    console.log("entrou")
-    chatMsgContainer.classList.remove('d-none')
-    joinChatSection.classList.add('d-none')
-    getChatMessage()
-    setInterval(getChatMessage, 6000)
-    setInterval(() => {
-      keepOnlineInChat(name)
-    }, 5000)
-  }
-  else {
-    console.log(joinChatSection)
-    const erroParagraph = joinChatSection.querySelector('.error-msg');
-    console.log(erroParagraph)
-    erroParagraph.textContent = 'Escolha outro usuario!'
-    erroParagraph.classList.remove('d-none')
+  if (msg.to === userName) {
+    insertElementIntoDOM('chat-msg__item private-msg', privateMsgTemplate)
   }
 }
 
-async function keepOnlineInChat(name) {
-  let options = {
+const insertErrorMsg = () => {
+  const erroParagraph = joinChatSection.querySelector('.error-msg');
+  erroParagraph.textContent = 'Escolha outro usuario!'
+  erroParagraph.classList.remove('d-none')
+}
+
+const insertElementIntoDOM = (className, template) => {
+  const msgItemElement = document.createElement('LI');
+
+  msgItemElement.setAttribute('class', className)
+  msgItemElement.innerHTML = template
+  chatMsgList.insertAdjacentElement('beforeend', msgItemElement)
+  
+  msgItemElement.scrollIntoView(true)
+}
+
+const loginInChat = (name, options) => {
+  chatMsgContainer.classList.remove('d-none')
+  joinChatSection.classList.add('d-none')
+
+  getChatMessage()
+  setInterval(getChatMessage, 6000)
+
+  setInterval(() => {
+    keepOnlineInChat(name, options)
+  }, 5000)
+}
+
+async function tryJoinChat(name) {
+  const options = {
     method: 'POST',
     headers: {
       "Content-Type": "application/json; charset=utf-8"
@@ -43,53 +68,31 @@ async function keepOnlineInChat(name) {
     body: `{"name": "${name}"}`
   }
 
+  const tryLoginChat = await fetch('https://mock-api.driven.com.br/api/v6/uol/participants', options)
+  const isPossibleJoin = tryLoginChat.status === 200
+
+  if (isPossibleJoin) {
+    loginInChat(options)
+  } else {
+    insertErrorMsg()
+  }
+}
+
+async function getChatMessage() {
+  const response = await fetch('https://mock-api.driven.com.br/api/v6/uol/messages')
+  const msgsData = await response.json()
+
+  chatMsgList.innerHTML = '' // limpando para previnir dos itens serem readicionados ?
+
+  msgsData.forEach(renderChatmsgByType)
+}
+
+async function keepOnlineInChat(options) {
   const keepUserOnline = await fetch('https://mock-api.driven.com.br/api/v6/uol/status', options)
 
   if (keepUserOnline.status === 200) {
     console.log('Mantendo Conexao')
   }
-
-  return keepUserOnline
-}
-
-async function getChatMessage() {
-  const userName = formElement.user.value
-  const response = await fetch('https://mock-api.driven.com.br/api/v6/uol/messages')
-  const msgsData = await response.json()
-
-  chatMsgList.innerHTML = '' // limpando para previnir dos itens serem readicionados ?
-  msgsData.forEach(msg => {
-    const msgItemElement = document.createElement('LI');
-    if (msg.type === 'status') {
-      const statusTemplate = `<span class="time-style">${msg.time}</span> <span class="text-bold">${msg.from}</span> ${msg.text}..`
-
-      msgItemElement.setAttribute('class', 'chat-msg__item status-msg')
-      msgItemElement.innerHTML = statusTemplate
-
-
-      chatMsgList.insertAdjacentElement('beforeend', msgItemElement)
-    } else if (msg.type === 'message') {
-      const statusTemplate = `<span class="time-style">${msg.time}</span> <span class="text-bold">${msg.from}</span>  para <span class="text-bold">${msg.to}</span> : ${msg.text}`
-
-      msgItemElement.setAttribute('class', 'chat-msg__item normal-msg')
-      msgItemElement.innerHTML = statusTemplate
-
-
-      chatMsgList.insertAdjacentElement('beforeend', msgItemElement)
-    } else {
-      const statusTemplate = `<span class="time-style">${msg.time}</span> <span class="text-bold">${msg.from}</span> reservadamente para <span class="text-bold">${msg.to}</span> : ${msg.text}`
-
-      msgItemElement.setAttribute('class', 'chat-msg__item private-msg')
-      if (msg.to === userName) {
-        msgItemElement.innerHTML = statusTemplate
-        chatMsgList.insertAdjacentElement('beforeend', msgItemElement)
-      }
-    }
-  })
-
-  const lastItem = document.querySelector('li.chat-msg__item:last-child')
-  lastItem.scrollIntoView()
-
 }
 
 async function sendMsgToChat(from, to, text, type) {
@@ -109,23 +112,17 @@ async function sendMsgToChat(from, to, text, type) {
 
   const sendPostRequisition = await fetch('https://mock-api.driven.com.br/api/v6/uol/messages', options)
   const getResponseInText = await sendPostRequisition.text()
-  if(getResponseInText != 'OK'){
+  if (getResponseInText != 'OK') {
     window.location.reload()
   }
-  else{
+  else {
     console.log("msg enviada")
     getChatMessage()
   }
-  
+
 }
 
-globalThis.sendMsgToChat = sendMsgToChat
-
-
-formElement.addEventListener('submit', event => {
-  event.preventDefault()
-  joinChatRoom(formElement.user.value)
-})
+formElement.addEventListener('submit', loginHandle)
 
 messageForm.addEventListener('submit', event => {
   event.preventDefault()
@@ -133,11 +130,7 @@ messageForm.addEventListener('submit', event => {
     sendMsgToChat(formElement.user.value, "todos", messageForm.message.value, "message")
     messageForm.message.value = '';
   }
-  
+
 })
 
-
-
-
-
-
+globalThis.sendMsgToChat = sendMsgToChat
